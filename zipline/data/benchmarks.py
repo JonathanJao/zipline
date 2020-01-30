190c2bc9db8f3eb5fc24589897c1e47ffdecc3cc
Jonathan Jao <fondyjao@gmail.com>
2020-01-29 22:14:43 -0600

    Updates benchmarks.py to pull local file for benchmark returns

--- zipline/data/benchmarks.py
+++ zipline/data/benchmarks.py
@@ -1,24 +1,10 @@
-#
-# Copyright 2013 Quantopian, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import numpy as np
 import pandas as pd
-import requests
+import pandas_datareader.data as pd_reader
 
-
-def get_benchmark_returns(symbol):
+def get_benchmark_returns(symbol, first_date, last_date):
     """
-    Get a Series of benchmark returns from IEX associated with `symbol`.
+    Get a Series of benchmark returns from Yahoo associated with `symbol`.
     Default is `SPY`.
 
     Parameters
@@ -26,17 +12,21 @@ def get_benchmark_returns(symbol):
     symbol : str
         Benchmark symbol for which we're getting the returns.
 
-    The data is provided by IEX (https://iextrading.com/), and we can
-    get up to 5 years worth of data.
+    The data is provided by Yahoo Finance
     """
-    r = requests.get(
-        'https://api.iextrading.com/1.0/stock/{}/chart/5y'.format(symbol)
-    )
-    data = r.json()
+    # data = pd_reader.DataReader(
+    #     symbol,
+    #     'yahoo',
+    #     first_date,
+    #     last_date
+    # )
+    data = pd.read_csv('/Users/jonatjao/pdev/trade/backtest/SPY.csv', index_col=0, parse_dates=['date'])
+    data = data['close']
 
-    df = pd.DataFrame(data)
+    data[pd.Timestamp('2008-12-15')] = np.nan
+    data[pd.Timestamp('2009-08-11')] = np.nan
+    data[pd.Timestamp('2012-02-02')] = np.nan
 
-    df.index = pd.DatetimeIndex(df['date'])
-    df = df['close']
+    data = data.fillna(method='ffill')
 
-    return df.sort_index().tz_localize('UTC').pct_change(1).iloc[1:]
+    return data.sort_index().tz_localize('UTC').pct_change(1).iloc[1:]
